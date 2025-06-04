ffdb5a330df15afdc97ee9fb6dbbdb49d9121ada
Tanishk7773 <tanishkrathore6@gmail.com>
2025-06-04 12:04:01 +0530

    new coomit

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,10 @@ function App() {
   return (
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-      style={{
-        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-      }}
+       style={{
+    backgroundImage: "url(/image/market.avif)",
+   
+  }}
     >
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
